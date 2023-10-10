e04588d84794ac25a957c278356b44cd3b555e04
Sihong He <Sihong.he@uconn.edu>
2023-10-09 22:56:30 -0400

    Update run_tasks.sh

--- run_tasks.sh
+++ run_tasks.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
-# 定义一个函数来运行任务
-run_task() {
-    env="$1"
-    beta_value="$2"
-    nohup python Fed_MBPG.py --env "$env" --beta "$beta_value" > "Fed_MBPG_${env}_${beta_value//.}p0.out" 2> "Fed_MBPG_${env}_${beta_value//.}p0.err" &
-    # 获取最新后台进程的PID
-    echo $!
+# Function to run general tasks for a specific environment three times
+run_tasks() {
+    env=$1
+    for run in {1..3}; do
+        for beta in 0.0 0.1 0.2 0.5 0.8 1.0; do
+            outfile="Fed_MBPG_${env}_${beta//./p}_${run}.out"
+            errfile="Fed_MBPG_${env}_${beta//./p}_${run}.err"
+            nohup python Fed_MBPG.py --env "$env" --beta "$beta" > "$outfile" 2> "$errfile" &
+        done
+        echo "${env} tasks for run ${run} started."
+        sleep 7200
+    done
 }
 
-beta_values=("0.0" "0.1" "0.2" "0.5" "0.8" "1.0")
-environments=("Walker" "Hopper" "HalfCheetah")
-
-for env in "${environments[@]}"; do
-    pids=()
-    # 启动该环境的所有任务
-    for beta in "${beta_values[@]}"; do
-        pids+=($(run_task "$env" "$beta"))
+# Function to run special tasks with --simple-avg option for a specific environment three times
+run_special_tasks() {
+    env=$1
+    for run in {1..3}; do
+        outfile="Fed_MBPG_${env}_1p0_avg_${run}.out"
+        errfile="Fed_MBPG_${env}_1p0_avg_${run}.err"
+        nohup python Fed_MBPG.py --env "$env" --beta 1.0 --simple-avg Yes > "$outfile" 2> "$errfile" &
     done
+    echo "Special ${env} tasks started."
+    sleep 3600
+}
 
-    # 等待该环境的所有任务完成
-    for pid in "${pids[@]}"; do
-        wait $pid
-    done
+# Run tasks
+run_tasks CartPole
+run_tasks Walker
+run_tasks Hopper
+run_tasks HalfCheetah
 
-    echo "$env tasks completed."
-done
+echo "All fed tasks scheduled."
 
-echo "All tasks completed."
+# Run special tasks
+run_special_tasks CartPole
+run_special_tasks Walker
+run_special_tasks Hopper
+run_special_tasks HalfCheetah
+
+echo "All avg tasks scheduled."
